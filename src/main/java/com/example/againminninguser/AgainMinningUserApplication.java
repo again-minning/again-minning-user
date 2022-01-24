d4097b02420609d7ba337a94ea282d80066dc96a
solchan98 <dev.sol0427@gmail.com>
2022-01-24 13:28:16 +0900

    feat: project init

---
+++ src/main/java/com/example/againminninguser/AgainMinningUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.againminninguser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgainMinningUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgainMinningUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/againminninguser/AgainMinningUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.againminninguser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgainMinningUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
