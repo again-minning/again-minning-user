619f790e79fa6e82e051cfe7d523d85dffbd15d5
solchan98 <dev.sol0427@gmail.com>
2022-02-03 17:28:28 +0900

    fix: #13 오타 수정

--- src/main/java/com/example/againminninguser/global/common/content/QuoteContent.java
+++ src/main/java/com/example/againminninguser/global/common/content/QuoteContent.java
@@ -10,5 +10,5 @@ public class QuoteContent {
     public static final String QUOTE_IS_EMPTY = "오늘의 명언이 비어있습니다.";
     public static final String QUOTE_UPDATE_OK = "오늘의 명언 업데이트를 성공하였습니다.";
 
-    public static final String QUOTE_UPDATE_ERROR = "오늘의 명언 업데이틀르 실패하였습니다.";
+    public static final String QUOTE_UPDATE_ERROR = "오늘의 명언 업데이트를 실패하였습니다.";
 }
